7e376574b8ca917cf4720f67a978947b29342f35
matiar_ <mateusz.sidor15@gmail.com>
2024-04-13 12:15:10 +0200

    fix: fixed .sql script

--- app/src/main/resources/db/migration/V2__auction.sql
+++ app/src/main/resources/db/migration/V2__auction.sql
@@ -2,8 +2,8 @@ CREATE TABLE auction
 (
     id          SERIAL          NOT NULL,
     name        VARCHAR(60)     NOT NULL,
-    startsAt    TIMESTAMP       NOT NULL,
-    endsAt      TIMESTAMP       NOT NULL,
+    starts_at    TIMESTAMP       NOT NULL,
+    ends_at      TIMESTAMP       NOT NULL,
     description VARCHAR(120)            ,
     price       NUMERIC(2)      NOT NULL,
     active      INDICATOR       NOT NULL,
